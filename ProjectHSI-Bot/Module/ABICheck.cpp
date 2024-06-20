483e85778cbc055bdd39c80820f91117e9ecaae5
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-20 10:48:57 +1000

    add documentation & rename namespace

--- ProjectHSI-Bot/Module/ABICheck.cpp
+++ ProjectHSI-Bot/Module/ABICheck.cpp
@@ -1,10 +1,23 @@
+/*!
+\file ProjectHSI-Bot/Module/ABICheck.cpp
+
+\brief This file implements the `ProjectHSI_Bot::Module::ABICheck` namespace.
+
+This module is used for checking the ABI Compatibility of a module.
+
+\see ProjectHSI-Bot/Module/ABICheck.hpp
+*/
+
 #include "ABICheck.hpp"
 #include <ProjectHSI-Bot-Shared-Types.h>
 #include <SDL.h>
 
+/*!
+\brief This typedef is a type for a function pointer of type ::abi_check.
+*/
 typedef bool (*abi_check_funct)(ProjectHSI_Bot_Shared_ABIVersion);
 
-bool ProjectHSI_Bot::Module::ABI_Check::performAbiCheck(void *moduleHandle) {
+bool ProjectHSI_Bot::Module::ABICheck::performAbiCheck(void *moduleHandle) {
 	abi_check_funct abi_check_func;
 
 	abi_check_func = reinterpret_cast< abi_check_funct >(SDL_LoadFunction(moduleHandle, "abi_check"));
