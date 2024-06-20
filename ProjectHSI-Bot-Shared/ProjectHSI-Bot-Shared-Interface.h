719dff0d2423cb634b89ffc9360d7a989171f6af
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-20 10:42:34 +1000

    add documentation

--- ProjectHSI-Bot-Shared/ProjectHSI-Bot-Shared-Interface.h
+++ ProjectHSI-Bot-Shared/ProjectHSI-Bot-Shared-Interface.h
@@ -1,3 +1,15 @@
+/*!
+\file ProjectHSI-Bot-Shared-Interface.h
+
+\brief This C header file does not contain any content as of this time.
+
+Including this header file is equivalent to:
+~~~{.h}
+#include "ProjectHSI-Bot-Shared-Interface-Types.h"
+#include "ProjectHSI-Bot-Shared.h"
+~~~
+*/
+
 #pragma once
 
 #include "ProjectHSI-Bot-Shared-Interface-Types.h"
