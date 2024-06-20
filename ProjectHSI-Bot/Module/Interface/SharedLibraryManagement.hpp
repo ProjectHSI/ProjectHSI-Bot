0445cfb723a049ab1bd9350af4421d2394f294d2
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-20 10:49:38 +1000

    add documentation

--- ProjectHSI-Bot/Module/Interface/SharedLibraryManagement.hpp
+++ ProjectHSI-Bot/Module/Interface/SharedLibraryManagement.hpp
@@ -1,8 +1,25 @@
+/*!
+\file ProjectHSI-Bot/Module/Interface/SharedLibraryManagement.hpp
+
+\brief This file supplies the ProjectHSI_Bot::Module::Interface::SharedLibraryManagement namespace.
+
+This module is used for managing interfaces in the "modules/interfaces" folder, relative to the base path (ProjectHSI_Bot::ApplicationContext::basePathChar).
+
+\see ProjectHSI-Bot/Module/Interface/SharedLibraryManagement.cpp
+*/
+
 namespace ProjectHSI_Bot {
 	namespace Module {
 		namespace Interface {
 			namespace SharedLibraryManagement {
+				/*!
+				\brief Loads all interfaces in "modules/interfaces", relative to the base path (ProjectHSI_Bot::ApplicationContext::basePathChar).
+				*/
 				void loadInterfaces();
+
+				/*!
+				\brief Unloads all interfaces loaded by ProjectHSI_Bot::Module::Interface::SharedLibraryManagement::loadInterfaces.
+				*/
 				void unloadInterfaces();
 			}
 		}
