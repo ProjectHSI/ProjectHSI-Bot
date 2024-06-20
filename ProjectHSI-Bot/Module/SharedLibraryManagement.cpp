032db6a06200f3a8578864f9864ae212af1c1c2b
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-20 10:47:04 +1000

    add file documentation

--- ProjectHSI-Bot/Module/SharedLibraryManagement.cpp
+++ ProjectHSI-Bot/Module/SharedLibraryManagement.cpp
@@ -1,3 +1,13 @@
+/*!
+\file ProjectHSI-Bot/Module/SharedLibraryManagement.cpp
+
+\brief This file implements the ProjectHSI_Bot::Module::SharedLibraryManagement namespace.
+
+This module is a wrapper for Interfaces & Engine loading.
+
+\see ProjectHSI-Bot/Module/SharedLibraryManagement.hpp
+*/
+
 #include "SharedLibraryManagement.hpp"
 #include "Interface/SharedLibraryManagement.hpp"
 
