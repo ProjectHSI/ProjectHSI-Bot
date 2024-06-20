48007d69a9e1121f840d84880318ad78a41ee023
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-20 10:39:00 +1000

    Modify documentation style

--- ProjectHSI-Bot-Shared/ProjectHSI-Bot-Shared.h
+++ ProjectHSI-Bot-Shared/ProjectHSI-Bot-Shared.h
@@ -14,18 +14,20 @@ This file also includes `ProjectHSI-Bot-Shared-Types.h`, where some type definit
 extern "C" {
 #endif
 
-	/**
-	 * \brief Used by the orchestrator to allow the engine/interface to check for ABI compatibility.
-	 * 
-	 * It is recommended to use the #ABI_CHECK macro to check the ABI version of the orchestrator, but the use of #ABI_CHECK is not required.
-	 * 
-	 * You **MUST NOT** initalize the engine in the abi_check function.
-	 * Engine initalization is done via a seperate (TBA) call, and the engine initalization MUST be done there,
-	 * instead in the abi_check function.
-	 * 
-	 * \see ABI_CHECK
-	 * \param[in] abiVersion The ABI version of the orchestrator.
-	 * \returns A boolean representing if the engine/interface is compatible with the given ABI version. If this is false, the engine will unload the DLL and will the interface will not be initalized.
+	/*!
+	\brief Used by the orchestrator to allow the engine/interface to check for ABI compatibility.
+	 
+	It is recommended to use the #ABI_CHECK macro to check the ABI version of the orchestrator, but the use of #ABI_CHECK is not required.
+	 
+	You **MUST NOT** initalize the engine in the abi_check function.
+	Engine initalization is done via a seperate (TBA) call, and the engine initalization MUST be done there,
+	instead in the abi_check function.
+
+	However, it is not forbidden to set internal flags for ABI functions if a custom implementation of abi_check outside of the #ABI_CHECK is used.
+	
+	\see ABI_CHECK
+	\param[in] abiVersion The ABI version of the orchestrator.
+	\returns A boolean representing if the engine/interface is compatible with the given ABI version. If this is false, the engine will unload the DLL and will the interface will not be initalized.
      */
 	bool abi_check(ProjectHSI_Bot_Shared_ABIVersion abiVersion);
 
