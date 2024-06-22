fe03411450f52082fc83961d941f8c18e3d558ce
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-22 18:25:37 +1000

    move & add functionality

--- ProjectHSI-Bot/Module/Interface/SharedLibraryManagement.cpp
+++ ProjectHSI-Bot/Module/Interface/SharedLibraryManagement.cpp
@@ -10,6 +10,7 @@ This module is used for managing interfaces in the "modules/interfaces" folder, 
 
 #include "../../ApplicationContext.hpp"
 #include "../ABICheck.hpp"
+#include "../SharedLibraryManagement.hpp"
 #include "SharedLibraryManagement.hpp"
 #include <cstdio>
 #include <filesystem>
@@ -18,53 +19,9 @@ This module is used for managing interfaces in the "modules/interfaces" folder, 
 #include <ProjectHSI-Bot-Shared-Types.h>
 #include <SDL.h>
 #include <string>
+#include <vector>
 
-/*!
-\brief The file extension of a shared object.
-
-This will be "dll" on Windows, and "so" in Unix.
-
-\note This is always shown as "so" in Doxygen. This is beacuse Doxygen does not define the `WIN32` macro which this variable relies on (even on Windows).
-This will update to the correct value on compilation.
-*/
-#ifdef WIN32
-constexpr const char *sharedObjectFileExtension("dll");
-#else
-constexpr const char *sharedObjectFileExtension("so");
-#endif
-
-/*!
-\brief Loads an interface given the path to it.
-
-This loads the interface into the interfaces vector, so there is no return value.
-
-\param[in] interfaceSharedObjectPath The path to the shared object.
-*/
-static void loadInterface(const std::filesystem::path &interfaceSharedObjectPath) {
-	printf("Loading %s...\n", interfaceSharedObjectPath.filename().generic_string().c_str());
-
-	if (interfaceSharedObjectPath.extension() != (std::string(".") + std::string(sharedObjectFileExtension))) {
-		fprintf(stderr, "File %s is not a shared object (or doesn't have the '.%s' extension). This file will be skipped.\n", interfaceSharedObjectPath.filename().generic_string().c_str(), sharedObjectFileExtension);
-
-		return;
-	}
-
-	void* object = SDL_LoadObject(interfaceSharedObjectPath.generic_string().c_str());
-
-// Win32 has it's own error messages for if a module fails to load, so we'll macro this out here.
-	if (object == NULL) {
-	#ifndef WIN32
-		printf("Failed to load %s due to an error thrown by SDL. This may not be a valid shared object.\nThe error thrown is: %s\n", interfaceSharedObjectPath.filename().generic_string().c_str(), SDL_GetError());
-	#endif
-		SDL_UnloadObject(object);
-	}
-
-	if (!ProjectHSI_Bot::Module::ABICheck::performAbiCheck(object))
-		SDL_UnloadObject(object);
-	else {
-		
-	}
-}
+std::vector<void *> interfaceSharedObjects = std::vector<void *>();
 
 void ProjectHSI_Bot::Module::Interface::SharedLibraryManagement::loadInterfaces() {
 	printf("Loading interfaces...\n");
@@ -73,7 +30,7 @@ void ProjectHSI_Bot::Module::Interface::SharedLibraryManagement::loadInterfaces(
 
 	if (std::filesystem::exists(interfacesPath)) {
 		for (const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator(interfacesPath)) {
-			loadInterface(entry.path());
+			interfaceSharedObjects.push_back(ProjectHSI_Bot::Module::SharedLibraryManagement::loadModule(entry.path()));
 		}
 	}
 
