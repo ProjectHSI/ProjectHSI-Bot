c30b89a4c5998bd1105f32ac49fcf06fd278eae5
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-06-22 18:23:55 +1000

    Move and add function for DRY

--- ProjectHSI-Bot/Module/SharedLibraryManagement.cpp
+++ ProjectHSI-Bot/Module/SharedLibraryManagement.cpp
@@ -8,8 +8,13 @@ This module is a wrapper for Interfaces & Engine loading.
 \see ProjectHSI-Bot/Module/SharedLibraryManagement.hpp
 */
 
-#include "SharedLibraryManagement.hpp"
+#include "ABICheck.hpp"
 #include "Interface/SharedLibraryManagement.hpp"
+#include "SharedLibraryManagement.hpp"
+#include <cstdio>
+#include <filesystem>
+#include <ProjectHSI-Bot-Shared-Types.h>
+#include <SDL_loadso.h>
 
 void ProjectHSI_Bot::Module::SharedLibraryManagement::loadModules() {
 	ProjectHSI_Bot::Module::Interface::SharedLibraryManagement::loadInterfaces();
@@ -17,4 +22,50 @@ void ProjectHSI_Bot::Module::SharedLibraryManagement::loadModules() {
 
 void ProjectHSI_Bot::Module::SharedLibraryManagement::unloadModules() {
 	ProjectHSI_Bot::Module::Interface::SharedLibraryManagement::unloadInterfaces();
+}
+
+void* ProjectHSI_Bot::Module::SharedLibraryManagement::loadModule(std::filesystem::path sharedObjectPath) {
+	printf("Loading %s...\n", sharedObjectPath.filename().generic_string().c_str());
+
+	if (sharedObjectPath.extension() != (std::string(".") + std::string(ProjectHSI_Bot::Module::SharedLibraryManagement::sharedObjectFileExtension))) {
+		fprintf(stderr, "File %s is not a shared object (or doesn't have the '.%s' extension). This file will be skipped.\n", sharedObjectPath.filename().generic_string().c_str(), sharedObjectFileExtension);
+
+		return nullptr;
+	}
+
+	void *sharedObjectHandle = SDL_LoadObject(sharedObjectPath.generic_string().c_str());
+
+	// Win32 has it's own error messages for if a module fails to load, so we'll macro this out here.
+	if (sharedObjectHandle == NULL) {
+	//#ifndef WIN32
+		printf("Failed to load %s due to an error thrown by SDL. This may not be a valid shared object.\nThe error thrown is: %s\n", sharedObjectPath.filename().generic_string().c_str(), SDL_GetError());
+	//
+		SDL_UnloadObject(sharedObjectHandle);
+		return nullptr;
+	}
+
+	if (!ProjectHSI_Bot::Module::ABICheck::performAbiCheck(sharedObjectHandle)) {
+		printf("Failed to load %s due to it's incompatibility with the current ABI version (%u.%u.%u).\n", sharedObjectPath.filename().generic_string().c_str(), ABI_VERSION_MAJOR, ABI_VERSION_MINOR, ABI_VERSION_PATCH);
+		SDL_UnloadObject(sharedObjectHandle);
+		return nullptr;
+	}
+
+	void *init_func = SDL_LoadFunction(sharedObjectHandle, "init");
+
+	if (init_func)
+		reinterpret_cast< ProjectHSI_Bot::Module::SharedLibraryManagement::init_funct >(init_func)();
+
+	return sharedObjectHandle;
+}
+
+void ProjectHSI_Bot::Module::SharedLibraryManagement::unloadModule(void *sharedObjectHandle) {
+
+	void *destroy_func = SDL_LoadFunction(sharedObjectHandle, "destroy");
+
+	if (destroy_func)
+		reinterpret_cast< ProjectHSI_Bot::Module::SharedLibraryManagement::destroy_funct >(destroy_func)();
+
+	SDL_UnloadObject(sharedObjectHandle);
+
+	return;
 }
--- ProjectHSI-Bot/Module/SharedLibraryManagement.hpp
+++ ProjectHSI-Bot/Module/SharedLibraryManagement.hpp
@@ -8,9 +8,25 @@ This module is a wrapper for Interfaces & Engine loading.
 \see ProjectHSI-Bot/Module/SharedLibraryManagement.cpp
 */
 
+#include <filesystem>
+
 namespace ProjectHSI_Bot {
 	namespace Module {
 		namespace SharedLibraryManagement {
+			/*!
+			\brief The file extension of a shared object.
+
+			This will be "dll" on Windows, and "so" in Unix.
+
+			\note This is always shown as "so" in Doxygen. This is beacuse Doxygen does not define the `WIN32` macro which this variable relies on (even on Windows).
+			This will update to the correct value on compilation.
+			*/
+		#ifdef WIN32
+			constexpr const char *sharedObjectFileExtension("dll");
+		#else
+			constexpr const char *sharedObjectFileExtension("so");
+		#endif
+
 			/*!
 			\brief Loads all modules. Loading behaviour is defined by the respective sub-functions this calls.
 
@@ -24,6 +40,36 @@ namespace ProjectHSI_Bot {
 			\see ProjectHSI_Bot::Module::Interface::SharedLibraryManagement::unloadInterfaces
 			*/
 			void unloadModules();
+
+			/*!
+			\brief Loads an module given the path to it.
+			
+			\warning The module handle returned may be nullptr. This will happen when the module could not be loaded for any reason.
+
+			\param[in] modulePath The path to the shared object.
+			\return A handle to module that was loaded.
+			As described in the SDL2 documentation, the type of the handle is opaque to the application.
+			*/
+			void* loadModule(std::filesystem::path modulePath);
+
+			/*!
+			\brief Loads an module given the path to it.
+
+			\note This function, unlike ProjectHSI_Bot::Module::SharedLibraryManagement::loadModule, will not fail (excluding catastrophic errors).
+
+			\param[in] moduleHandle A handle to the module to be unloaded.
+			*/
+			void unloadModule(void* moduleHandle);
+
+			/*!
+			\brief This typedef is a type for a function pointer of type ::init.
+			*/
+			typedef void (*init_funct)();
+
+			/*!
+			\brief This typedef is a type for a function pointer of type ::destroy.
+			*/
+			typedef void (*destroy_funct)();
 		}
 	}
 }
