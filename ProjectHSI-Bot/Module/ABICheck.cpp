f87b52e1171937f7c55b899054feb80a023b2524
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-07-13 19:39:50 +1000

    refactor & use logging functions

--- ProjectHSI-Bot/Module/ABICheck.cpp
+++ ProjectHSI-Bot/Module/ABICheck.cpp
@@ -8,10 +8,12 @@ This module is used for checking the ABI Compatibility of a module.
 \see ProjectHSI-Bot/Module/ABICheck.hpp
 */
 
+#include "../Logger.hpp"
 #include "ABICheck.hpp"
 #include <cstdint>
 #include <ProjectHSI-Bot-Shared-Types.h>
 #include <SDL_loadso.h>
+#include <stdio.h>
 
 uint_least8_t ProjectHSI_Bot::Module::ABICheck::performAbiCheck(void *moduleHandle) {
 	if (!SDL_LoadFunction(moduleHandle, "projecthsi_bot_module_prescene"))
@@ -22,5 +24,11 @@ uint_least8_t ProjectHSI_Bot::Module::ABICheck::performAbiCheck(void *moduleHand
 	if (!abi_check_func)
 		return false;
 
-	return static_cast<uint_least8_t>(!reinterpret_cast< ProjectHSI_Bot::Module::ABICheck::abi_check_funct >(abi_check_func)({ABI_VERSION_MAJOR, ABI_VERSION_MINOR, ABI_VERSION_PATCH}));
+	ProjectHSI_Bot::CLogger::log(ProjectHSI_Bot::CLogger::LogLevel::INFORMATION, "Checking module ABI...");
+
+	bool abiCheckResult = static_cast< uint_least8_t >(!reinterpret_cast< ProjectHSI_Bot::Module::ABICheck::abi_check_funct >(abi_check_func)({ABI_VERSION_MAJOR, ABI_VERSION_MINOR, ABI_VERSION_PATCH}));
+
+	ProjectHSI_Bot::CLogger::log(ProjectHSI_Bot::CLogger::LogLevel::INFORMATION, "Checked module ABI.");
+
+	return abiCheckResult;
 }
