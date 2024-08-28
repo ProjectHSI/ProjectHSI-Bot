3484964bcc193a4a81471e79e0c2e790bbc1cd0a
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2024-08-28 13:46:21 +1000

    update

--- ProjectHSI-Bot/Logger.hpp
+++ ProjectHSI-Bot/Logger.hpp
@@ -21,6 +21,7 @@ This file supplies the `ProjectHSI_Bot::CLogger` namespace and implements the in
 #include <string_view>
 #include <regex>
 #include <chrono>
+#include <version>
 //#include "Logger.cpp"
 
 namespace ProjectHSI_Bot {
@@ -148,7 +149,7 @@ namespace ProjectHSI_Bot {
 		void log(const LogStruct &logStruct, const std::string& logMessage, const std::string& logSource, Args... args) {
 		// (ascii prefix) [(time)] [(source)] ((level)): (text)\n
 
-		//#if 1
+		#ifdef __cpp_lib_format
 			const std::regex printfReplacementCharacter("\%*");
 
 			const std::string stdFormatCompat = std::regex_replace(logMessage, printfReplacementCharacter, "{}");
@@ -162,15 +163,14 @@ namespace ProjectHSI_Bot {
 				logStruct.logPrefix.data(),
 				logMessage.data());
 
-			/*
 		#else
-		#pragma warning( push )
-		#pragma warning( disable : 4774 )
+		//#pragma warning( push )
+		//#pragma warning( disable : 4774 )
 			int nBuffer = snprintf(nullptr, 0, logMessage.data(), &args...);
 			std::string buffer {};
 			buffer.resize(static_cast<std::basic_string<char, std::char_traits<char>, std::allocator<char>>::size_type>(nBuffer) + 1);
 			snprintf(buffer.data(), nBuffer, logMessage.data(), &args...);
-		#pragma warning( pop )
+		//#pragma warning( pop )
 
 			fprintf(
 				logStruct.logLevel < 0 ? stderr : stdout,
@@ -179,7 +179,6 @@ namespace ProjectHSI_Bot {
 				logStruct.logPrefix.data(),
 				buffer.data());
 		#endif
-		*/
 		}
 
 		/*!
